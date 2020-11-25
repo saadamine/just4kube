ea650bd5550404204fb801fe6712ee3a8570e5cc
Amine SAADAOUI <amine.saadaoui@easyteam.fr>
2020-11-25 22:32:35 +0100

    [api] create configmap

--- controllers/friend_controller.go
+++ controllers/friend_controller.go
@@ -19,12 +19,17 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
 
 
 	urlv1alpha1 "gytigyg.io/api/v1alpha1"
@@ -35,10 +40,33 @@ type FriendReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
+	Properties FriendProperties
+
+}
+type FriendProperties struct {
+	AgentVersion string
 }
 
 // +kubebuilder:rbac:groups=url.gytigyg.io,resources=friends,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=url.gytigyg.io,resources=friends/status,verbs=get;update;patch
+func (f *FriendProperties) NewConfigMapForFriend(friend *urlv1alpha1.Friend) *corev1.ConfigMap {
+	labels := map[string]string{
+		"app": friend.Name,
+	}
+
+	configMap := &v1.ConfigMap{
+		ObjectMeta:  metav1.ObjectMeta {
+			Name:      "gytigyg-"+friend.Name,
+			Namespace: friend.Namespace,
+			Labels:    labels,
+		},
+		Data: map[string]string{
+			"uri": string(friend.Spec.Uri),
+		},
+	}
+	return configMap
+}
+
 
 func (r *FriendReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
@@ -49,32 +77,50 @@ func (r *FriendReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	var friend urlv1alpha1.Friend
 
 	if err := r.Get(ctx, req.NamespacedName, &friend); err != nil {
-		log.Error(err, "unable to fetch Store")
+		log.Error(err, "unable to fetch Friend")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+
 	fmt.Println(friend.Spec.Uri)
 	if friend.Spec.Uri != "https://url.gytigyg.io/amine"{
 		fmt.Println("URI doesn't match")
+		// // Configmap creation failed
+		// friend.Status.Active = "Failed"
+		// if err := r.Status().Update(ctx, &friend); err != nil {
+		// 	r.Log.Error(err, "unable to update friend status")
+		// 	return ctrl.Result{}, err
+		// }
+		// return ctrl.Result{}, nil
 	}
 
 	if friend.Spec.Uri == "https://url.gytigyg.io/amine" {
-		labels := map[string]string{
-			"app": req.NamespacedName.Name,
-		}
+		ctx := context.Background()
+		configmapName := types.NamespacedName{Name: "gytigyg-"+friend.Name, Namespace: friend.Namespace}
+		configmap := &corev1.ConfigMap{}
 
-		configMap := &v1.ConfigMap{
-			ObjectMeta:  metav1.ObjectMeta {
-				Name:      "gytigyg-"+friend.Name,
-				Namespace: req.Namespace,
-				Labels:    labels,
-			},
-			Data: map[string]string{
-				"uri": string(friend.Spec.Uri),
-			},
+		err := r.Client.Get(ctx, configmapName, configmap)
+		if err != nil && errors.IsNotFound(err) {
+
+			r.Log.Info("Creating a new configmap", "Friend.Namespace", friend.Namespace, "friend.Name", "gytigyg-"+friend.Name)
+			newConfigMap := r.Properties.NewConfigMapForFriend(&friend)
+			if err := controllerutil.SetControllerReference(&friend, newConfigMap, r.Scheme); err != nil {
+				return ctrl.Result{}, err
+			}
+			if err := r.Client.Create(ctx, newConfigMap); err != nil {
+				return ctrl.Result{}, err
+			}
 		}
-		if err := r.Client.Create(ctx, configMap); err != nil {
+		if err != nil {
 			return ctrl.Result{}, err
 		}
+		
+		// Configmap creation failed
+		friend.Status.Active = "Success"
+		if err := r.Status().Update(ctx, &friend); err != nil {
+			r.Log.Error(err, "unable to update friend status")
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
 	}
 
 	return ctrl.Result{}, nil
